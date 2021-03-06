8a870396d99cae742d36de76e6dd76c0a83fd1f8
David Cary <david@milfordguide.app>
2021-03-06 16:37:08 +1300

    made a script to automatically make a trap catch checklist

---
+++ Doubtful_Sound/parseTemplate.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import gpxpy
+import gpxpy.gpx
+
+# ./parseTemplate.py > Template_Deep_Cove_Outdoor_Education_Trust_Conservation_Initiative.csv
+# ----------------------------------------------------------------------------------
+
+gpx_file = open('Waypoints_Doubtful_Sound.gpx', 'r')
+
+gpx = gpxpy.parse(gpx_file)
+print('Line, Trap Number, Type, Catch, Comment -------------------')
+for waypoint in gpx.waypoints:
+    print( waypoint.description+', '+waypoint.name+', '+waypoint.comment+',,')
+print(',,TEAM, DATE, Please send to:')
+print(',,,, stanleymulvany@gmail.com')
+print(',,,, S Mulvany PO Box 6071')
+print(',,,, Invercargill 9841')
